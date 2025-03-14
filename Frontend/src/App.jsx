857c7d062b5ee75c7bc27124677b94bd06f9bbd1
Bhargav Govind Katkam <bhargavkatkam0@gmail.com>
2025-03-14 15:37:56 +0530

    Updated Navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,12 +3,14 @@ import { Toaster as Sonner } from "./components/ui/sonner";
 import { TooltipProvider } from "./components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Index from "./pages/Index";
 import Questions from "./pages/Questions";
 import QuestionDetail from "./pages/QuestionDetail";
 import Profile from "./pages/Profile";
 import Auth from "./pages/Auth";
-import Colleges from "./pages/Colleges";
+import CollegesList from "./pages/CollegesList";
+import CollegePage from "./pages/CollegePage";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -19,13 +21,15 @@ const App = () => (
       <Toaster />
       <Sonner />
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/login" element={<Auth />} />
           <Route path="/signup" element={<Auth />} />
           <Route path="/questions" element={<Questions />} />
           <Route path="/question/:id" element={<QuestionDetail />} />
-          <Route path="/colleges" element={<Colleges />} />
+          <Route path="/colleges" element={<CollegesList />} />
+          <Route path="/college/:id" element={<CollegePage />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
@@ -34,4 +38,4 @@ const App = () => (
   </QueryClientProvider>
 );
 
-export default App; 
+export default App;
