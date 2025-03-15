b1cd19376724400640afd976f210fcd090a12e91
Udaymore741 <orionlee741@gmail.com>
2025-03-15 13:58:30 +0530

    updated adpp.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -13,8 +13,47 @@ import CollegesList from "./pages/CollegesList";
 import CollegePage from "./pages/CollegePage";
 import NotFound from "./pages/NotFound";
 import Activity from "./pages/Activity";
+import AdminDashboard from "./pages/admin/Dashboard";
+import AdminColleges from "./pages/admin/Colleges";
+import AdminUsers from "./pages/admin/Users";
+import AdminReports from "./pages/admin/Reports";
+import AdminLayout from "./components/layouts/AdminLayout";
+import { useLocation } from "react-router-dom";
 
 const queryClient = new QueryClient();
+
+const AppContent = () => {
+  const location = useLocation();
+  const isAdminRoute = location.pathname.startsWith("/admin");
+  const isAuthRoute = location.pathname === "/login" || location.pathname === "/signup";
+
+  return (
+    <>
+      {!isAdminRoute && !isAuthRoute && <Navbar />}
+      <Routes>
+        {/* Admin Routes */}
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<AdminDashboard />} />
+          <Route path="colleges" element={<AdminColleges />} />
+          <Route path="users" element={<AdminUsers />} />
+          <Route path="reports" element={<AdminReports />} />
+        </Route>
+
+        {/* Public Routes */}
+        <Route path="/" element={<Index />} />
+        <Route path="/login" element={<Auth />} />
+        <Route path="/signup" element={<Auth />} />
+        <Route path="/questions" element={<Questions />} />
+        <Route path="/question/:id" element={<QuestionDetail />} />
+        <Route path="/colleges" element={<CollegesList />} />
+        <Route path="/college/:id" element={<CollegePage />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/activity" element={<Activity />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+};
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
@@ -22,19 +61,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/login" element={<Auth />} />
-          <Route path="/signup" element={<Auth />} />
-          <Route path="/questions" element={<Questions />} />
-          <Route path="/question/:id" element={<QuestionDetail />} />
-          <Route path="/colleges" element={<CollegesList />} />
-          <Route path="/college/:id" element={<CollegePage />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/activity" element={<Activity />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <AppContent />
       </Router>
     </TooltipProvider>
   </QueryClientProvider>
