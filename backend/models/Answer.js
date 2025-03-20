e72933c9b2892f710c2a46ddbc0a90f673a2d9a8
Udaymore741 <orionlee741@gmail.com>
2025-03-20 19:53:41 +0530

    created answer model

---
+++ backend/models/Answer.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const answerSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  question: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Question',
+    required: true
+  },
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  isAccepted: {
+    type: Boolean,
+    default: false
+  },
+  comments: [{
+    content: {
+      type: String,
+      required: true
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
+  }]
+}, {
+  timestamps: true
+});
+
+// Index for faster queries
+answerSchema.index({ question: 1 });
+answerSchema.index({ author: 1 });
+
+module.exports = mongoose.model('Answer', answerSchema); 
