a6c636939b68dd452584a301fd0b5db06023feb6
Udaymore741 <orionlee741@gmail.com>
2025-03-20 20:01:50 +0530

    created socket.io service

---
+++ backend/services/socketService.js
@@ -0,0 +1,114 @@
+const socketIO = require('socket.io');
+
+let io;
+
+const initializeSocket = (server) => {
+  io = socketIO(server, {
+    cors: {
+      origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+      methods: ['GET', 'POST'],
+      credentials: true
+    }
+  });
+
+  io.on('connection', (socket) => {
+    console.log('New client connected');
+
+    // Join a college room
+    socket.on('join-college', (collegeId) => {
+      socket.join(`college-${collegeId}`);
+      console.log(`Client joined college room: ${collegeId}`);
+    });
+
+    // Leave a college room
+    socket.on('leave-college', (collegeId) => {
+      socket.leave(`college-${collegeId}`);
+      console.log(`Client left college room: ${collegeId}`);
+    });
+
+    // Join a question room
+    socket.on('join-question', (questionId) => {
+      socket.join(`question-${questionId}`);
+      console.log(`Client joined question room: ${questionId}`);
+    });
+
+    // Leave a question room
+    socket.on('leave-question', (questionId) => {
+      socket.leave(`question-${questionId}`);
+      console.log(`Client left question room: ${questionId}`);
+    });
+
+    socket.on('disconnect', () => {
+      console.log('Client disconnected');
+    });
+  });
+
+  return io;
+};
+
+const emitNewQuestion = (collegeId, question) => {
+  if (io) {
+    io.to(`college-${collegeId}`).emit('new-question', question);
+  }
+};
+
+const emitNewAnswer = (questionId, answer) => {
+  if (io) {
+    io.to(`question-${questionId}`).emit('new-answer', answer);
+  }
+};
+
+const emitQuestionUpdate = (collegeId, question) => {
+  if (io) {
+    io.to(`college-${collegeId}`).emit('question-updated', question);
+  }
+};
+
+const emitAnswerUpdate = (questionId, answer) => {
+  if (io) {
+    io.to(`question-${questionId}`).emit('answer-updated', answer);
+  }
+};
+
+const emitQuestionDelete = (collegeId, questionId) => {
+  if (io) {
+    io.to(`college-${collegeId}`).emit('question-deleted', questionId);
+  }
+};
+
+const emitAnswerDelete = (questionId, answerId) => {
+  if (io) {
+    io.to(`question-${questionId}`).emit('answer-deleted', answerId);
+  }
+};
+
+const emitLikeUpdate = (questionId, likes) => {
+  if (io) {
+    io.to(`question-${questionId}`).emit('likes-updated', likes);
+  }
+};
+
+const emitAnswerLikeUpdate = (questionId, answerId, likes) => {
+  if (io) {
+    io.to(`question-${questionId}`).emit('answer-likes-updated', { answerId, likes });
+  }
+};
+
+const emitNewComment = (questionId, answerId, comment) => {
+  if (io) {
+    io.to(`question-${questionId}`).emit('new-comment', { answerId, comment });
+  }
+};
+
+module.exports = {
+  initializeSocket,
+  emitNewQuestion,
+  emitNewAnswer,
+  emitQuestionUpdate,
+  emitAnswerUpdate,
+  emitQuestionDelete,
+  emitAnswerDelete,
+  emitLikeUpdate,
+  emitAnswerLikeUpdate,
+  emitNewComment
+}; 
